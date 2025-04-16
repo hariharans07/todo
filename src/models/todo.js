7d24bfb34600a8a21f77deda373edd5a9b81c6c3
hariharans07 <hariharansekarselvam@gmail.com>
2025-04-16 13:00:39 +0530

    Auth

--- src/models/todo.js
+++ src/models/todo.js
@@ -2,12 +2,27 @@ const mongoose = require('mongoose');
 
 const todoSchema = new mongoose.Schema({
     title:String,
-    descriptionn:String,
+    description:String,
 },{
     timestamps:true
 });
 
-const todoModel=mongoose.mongoose.model('todo',todoSchema);
+const authSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+const todoModel=mongoose.model('todo',todoSchema);
+const authModel=mongoose.model('auth',authSchema);
 
 async function createTodo(title, description) {
     const created=await todoModel.create({
@@ -30,6 +45,56 @@ async function updateTodo(_id, title, description) {
     return updated;
 }
 
+async function getTodo() {
+    const todos = await todoModel.find();
+    return todos;
+  }
+
+//pagination
+
+async function pageTodo(page, limit) {
+    const skip =(page -1)*limit;
+    const todo = await todoModel.find().skip(Number(skip)).limit(Number(limit));
+    return todo;
+}
+
+//signup function (account creation)
+async function signup(username, password) {
+    try {
+        if (!username || !password) {
+            throw new Error('Username and password are required');
+        }
+        
+        const auth = await authModel.create({
+            username,
+            password
+        });
+        
+        return {
+            _id: auth._id,
+            username: auth.username,
+            createdAt: auth.createdAt
+        };
+    } catch (error) {
+        if (error.code === 11000) {
+            throw new Error('Username already exists');
+        }
+        throw error;
+    }
+}
+
+
+async function getAuth() {
+    const auths = await authModel.find();
+    return auths;
+  }
+
+//login function
+async function loginID(username,password){
+    const login = await authModel.find({username,password});
+    return login;
+}
+
 module.exports={
-    createTodo,deleteTodo,updateTodo
+    createTodo,deleteTodo,updateTodo,getTodo,pageTodo,signup,loginID,getAuth
 }
--- src/routes/todo.js
+++ src/routes/todo.js
@@ -1,5 +1,5 @@
 const express=require('express');
-const {createTodo,deleteTodo,updateTodo}=require('../models/todo');
+const {createTodo,deleteTodo,updateTodo,getTodo,pageTodo,signup,loginID,getAuth}=require('../models/todo');
 const router=express.Router();
 
 router.post('/',async(req,res)=>{
@@ -21,5 +21,47 @@ router.put('/:id',async(req,res)=>{
     const todo=await updateTodo(_id,title,description);
     res.send(todo);
 })  
+
+router.get('/',async (req,res)=>{
+    const todos = await getTodo()
+    res.send(todos)
+})
+
+//pagination
+router.get('/page', async (req, res) => {
+    const { page, limit} = req.body;
+    const todo = await pageTodo(page, limit);
+    res.send(todo);
+});
+
+//user authentication 
+router.post('/signup', async(req, res) => {
+    try {
+        const {username, password} = req.body;
+        const user = await signup(username, password);
+        res.json("User created successfully");
+    } catch (error) {
+        res.json("Signup failed");
+    }
+});
+
+router.get('/signup',async (req,res)=>{
+    const auths = await getAuth()
+    res.send(auths)
+})
+
+
+router.post('/login',async(req, res)=>{
+    const {username,password} = req.body;
+    console.log({body: req.body});  
+    const login =await loginID(username,password);
+    if (login.length<=0){
+        res.json("invalid credentials");
+    }
+    else{
+        res.json("Good!!")
+    }
+});
+
 
 module.exports=router;
